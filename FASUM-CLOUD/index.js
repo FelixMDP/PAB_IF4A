3bded84bb9bc0c024f7d6135bf6399bee49ae01c
FelixMDP <felixgunawan1234@gmail.com>
2025-05-14 09:31:27 +0700

    P

---
+++ FASUM-CLOUD/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const serviceAccount = require('./serviceAccountKey.json');
+const cors = require('cors');
+require('dotenv/config');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    });
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    app.post('send-notification', async (req, res) => {
+        const{token, title, body} = req.body;
+
+        if(!token || !title || !body) {
+            return res.status(400).json('token, title, body wajib diisi');
+        }
+        const message = {
+            notification: {
+                title: title,
+                body: body,
+            },
+        
+        };                                              
+        try {
+            const response = await admin.messaging().send(message);
+            res.status(200).json({
+                success: true,
+                response: response,
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                error: error.message,
+            });
+        }
+    });
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+        console.log('Server running on ${PORT}');
+     });
